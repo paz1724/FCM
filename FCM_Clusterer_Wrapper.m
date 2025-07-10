c928d40e2f1c39bcd9c5903f2eb72014dacc2bec
paz1724 <paz1724@gmail.com>
2025-07-10 22:41:27 +0300

    FCM now works

--- FCM_Clusterer_Wrapper.m
+++ FCM_Clusterer_Wrapper.m
@@ -2,27 +2,25 @@ function FCM_Clusterer_Wrapper()
 % Generate demo data
 seed = 1724;
 rng(seed);
-X1 = randn(100,2)*0.3 + [1,-1];
-X2 = randn(120,2)*0.4 + [-1,2];
-X3 = randn( 80,2)*0.5 + [2, 1];
-X4 = randn( 20,2)*0.5 + [3, 4];
+X1 = randn(5,2)*0.3 + [1,-1];
+X2 = randn(4,2)*0.4 + [-1,2];
+X3 = randn( 3,2)*0.5 + [2, 1];
+X4 = randn( 2,2)*0.5 + [3, 4];
 X  = [X1; X2; X3; X4];
 
 %% Create clusterer with automatic c‐selection
 oFCM = cFCM( ...
     'autoSelect',       true, ...
     'clusterRange',     1:6, ...
+    'minClusterSize',   3, ...
     'validityIndex',    'XB', ...
     'outlierMethod',    'iqr', ...
-    'outlierIQRFactor', 1.5);
+    'outlierIQRFactor', 1.2);
 
 % profile on
 
 %% Fit
 oFCM.Fit(X);
-
-%% Prune
-oFCM.Prune_outliers(X);
 
 %% Plot
 oFCM.Plot_results(X);
